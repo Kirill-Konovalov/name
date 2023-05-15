778b0c8467113c2f5416c1c3af1822302f29bdd2
Kirill-Konovalov <96065474+Kirill-Konovalov@users.noreply.github.com>
2023-05-15 14:12:09 +0400

    .

---
+++ name-main/TEST BAckend developer/js/mainn.js
@@ -0,0 +1,150 @@
+MassiveOfQuestions = ['Какой язык программирования применяется для работы с базами данных MySQL?', "Выберите язык, который вы знайте", "Чем отличается функция от метода в Python?", "Как проверить, что две переменные ссылаются на один и тот же объект?",
+'Каким образом можно выбрать все записи из таблицы "users", где данные в поле "username" начинаются с буквы "A"?', 
+'Каким образом можно выбрать максимальное значение поля "age" из таблицы "users"?', 'Каким образом можно создать новую таблицу "products" с полями "id", "name" и "price"?', 'Каким образом можно удалить все записи из таблицы "orders", где статус равен "canceled"?', "Какой тип нереляционных баз данных наиболее часто используется?", "Какой тип нереляционных баз данных наиболее эффективен для хранения данных в виде JSON-документов?", "В чем отличие нереляционных баз данных от реляционных?", "Какие из этих баз данных можно использовать для хранения больших объемов данных и обеспечения горизонтального масштабирования?", "Каким образом можно получить данные из нереляционной базы данных?", 
+"С помощью документо-ориентированных библиотек", "Что такое полиморфизм?", "Что такое инкапсуляция?", "Что такое абстрактный класс?", "Что такое интерфейс?", "Что означает аббревиатура REST?", "Какие операции HTTP используются в RESTful API?", "Что такое ресурс в RESTful API? ", "Какой формат данных обычно используется в RESTful API для передачи информации?", "Что означает код ответа HTTP 404 в RESTful API?", "Что означает аббревиатура VCS?", "Какая команда используется для создания новой ветки в git?", "3.	Какие команды используются для отправки изменений из локального репозитория в удаленный?", "4.	Какая команда используется для объединения двух веток в git?", "5.	Что произойдет, если в удаленный репозиторий были добавлены изменения, которых нет в локальной версии, и была использована команда git pull?"]
+
+MassiveOfAnswers = [['SQL', 'Java', 'Python', 'Ruby', 'PHP'],["Python", "Java", "Php", "C#", "Ruby"],['A', 'B', 'C', 'D', "E"], ['Java', 'B', 'C', 'D', "E"],
+["SELECT * FROM users WHERE username LIKE 'A%'","SELECT * FROM users WHERE username = 'A%'", "SELECT * FROM users WHERE username LIKE '%A'", "SELECT * FROM users WHERE username = '%A'", "SELECT * FROM users WHERE username LIKE '%A%'"],
+["SELECT MAX('age') FROM users", "SELECT MAX(age) FROM users", "SELECT COUNT(age) FROM users", "SELECT AVG(age) FROM users", "SELECT SUM(age) FROM users"], ["CREATE TABLE products (id INT, name VARCHAR(255), price INT)", "CREATE TABLE id, name, price FROM products", "INSERT INTO products (id, name, price)", "SELECT products (id, name, price)", "UPDATE products SET id, name, price"], 
+["DELETE FROM orders WHERE status = 'canceled'", "TRUNCATE orders WHERE status = 'canceled'", "UPDATE orders SET status = 'completed' WHERE status = 'canceled'", "DROP orders WHERE status = 'canceled'", "SELECT orders WHERE status = 'canceled'"],
+["Ключ-значение", "Графовые базы данных", "Графовые базы данных", "Столбцовые базы данных", "Реляционные базы данных"], ["Ключ-значение", "Графовые базы данных", "Семейства документов", "Столбцовые базы данных", "Реляционные базы данных"], ["Нереляционные базы данных не поддерживают SQL", "Нереляционные базы данных не имеют жесткой структуры", "Нереляционные базы данных не могут хранить связанные данные", "Нереляционные базы данных не предназначены для хранения больших объемов данных", "Нереляционные базы данных применяются только для хранения текстовых данных"], ["Ключ-значение", "Семейства документов", "Столбцовые базы данных", "Реляционные базы данных", "Все вышеперечисленные базы данных"],
+["SQL-запросы", "Cypher-запросы", "CQL-запросы", "С помощью документо-ориентированных библиотек", "С помощью ORM (Object-Relational Mapping)"],
+["Возможность создания нескольких объектов одного класса", "Возможность изменения параметров объекта во время выполнения программы", "Возможность получения свойств и методов родительского класса в дочернем классе", "Механизм, позволяющий методам дочернего класса вызывать методы родительского класса", "Механизм, позволяющий изменять связи между объектами"], ["Возможность создания объектов на основе абстрактных классов или интерфейсов", "Возможность доступа к объектам класса из любой части программы", "Возможность переопределения методов родительского класса в дочерних классах", "Возможность использования одного и того же метода для разных типов данных", "Возможность создания объектов с динамическим типом данных"], ["Возможность создания классов с несколькими уровнями доступа к переменным и методам", "Возможность создания объектов на основе абстрактных классов или интерфейсов", "Возможность изменения параметров объекта во время выполнения программы", "Возможность доступа к объектам класса из любой части программы", "Механизм, позволяющий методам дочернего класса вызывать методы родительского класса"], ["Класс, у которого не может быть создан объект", "Класс, который не может содержать абстрактных методов", "Класс, который содержит только статические методы", "Класс, который может наследоваться только один раз", "Класс, который не может быть унаследован"], ["Класс, который может содержать только конструкторы", "Способ связи объектов между собой", "Язык программирования для создания веб-приложений", "Описание методов, которые должны быть реализованы классом, реализующим интерфейс", "Способ передачи параметров между методами одного класса"]
+["Расширенный стандарт передачи состояния", "Представление состояния передачи", "Представление перенаправления", "Передача состояния с помощью HTTP", "Представление передачи состояни"], ["GET, POST, PUT, DELETE", "GET, POST, UPDATE, DELETE", "INSERT, SELECT, UPDATE, DELETE", "REQUEST, RESPONSE, INSERT, DELETE", "GET, REQUEST, RESPONSE, DELETE"], ["Пароль для доступа к API", "URL-адрес, по которому можно получить данные", "Данные, которые возвращаются в ответ на запрос", "Алгоритм шифрования, используемый в API", "Метод, который используется для запроса данных"], ["XML", "JSON", "HTML", "CSV", "TXT"], ["Ресурс не найден", "Необходимо войти в систему для доступа к ресурсу", "Недостаточно прав для выполнения операции", "Сервер временно недоступен", "Ошибка валидации переданных данных"]]
+
+pythonQuestions = ["Чем отличается функция от метода в Python?", "Как проверить, что две переменные ссылаются на один и тот же объект?"]
+pythonAnswers = [['A', 'B', 'C', 'D', "E"], ['A', 'B', 'C', 'D', "E"]]
+ArrayPythonAnsew = ['A', 'E']
+
+
+
+
+ArrayAnswer = ['PHP','Python', 'A', 'E', "SELECT * FROM users WHERE username LIKE 'A%'", "LECT MAX(age) FROM users", "CREATE TABLE products (id INT, name VARCHAR(255), price INT)", "DELETE FROM orders WHERE status = 'canceled'", "Семейства документов", "Семейства документов", "Нереляционные базы данных не имеют жесткой структуры", "Все вышеперечисленные базы данных", "С помощью документо-ориентированных библиотек", "Возможность получения свойств и методов родительского класса в дочернем классе", "Возможность использования одного и того же метода для разных типов данных", "Возможность создания классов с несколькими уровнями доступа к переменным и методам", "Класс, у которого не может быть создан объект", "Описание методов, которые должны быть реализованы классом, реализующим интерфейс", "Передача состояния с помощью HTTP", "GET, POST, PUT, DELETE", "URL-адрес, по которому можно получить данные", "JSON", "Ресурс не найден"] //Правильные ответы теста всего
+
+let scores = {
+    rights: 0,
+    wrongs: 0
+}
+
+
+let listAnswer = document.getElementsByTagName('input')
+const further = document.querySelector('.btnFurther')
+const back = document.querySelector('.btnBack')
+let headline = document.querySelector('h1')
+let numberQuestion = document.querySelector('#numberQuestion')
+const result = document.querySelector('#result')
+const test = document.querySelector('#test')
+let wrongsAnswer = []
+let savedAnswers = []
+let answerGiven = false;
+let choice =  ''
+let step = 0
+
+
+
+let listAnswerSpan = document.getElementsByTagName('span')
+
+
+
+window.onload = start 
+
+function start(){ //выставляю вопросы
+    headline.innerText = MassiveOfQuestions[step]
+    for( let p = 0; p < listAnswer.length; p++){
+        console.log(MassiveOfAnswers[step][p], step, p);
+        listAnswerSpan[p].innerHTML = MassiveOfAnswers[step][p]
+    }
+}
+
+
+const furtherQuestion = () =>{
+    for(let i = 0; i < listAnswer.length; i++){
+        if(listAnswer[i].checked == true){
+            answerGiven = true;
+            console.log(listAnswerSpan[i].innerText);
+            console.log(ArrayAnswer[step]);
+            if(listAnswerSpan[i].innerText == ArrayAnswer[step]){
+                scores.rights = scores.rights + 1
+                savedAnswers.push('rights')
+            }
+            else{
+                scores.wrongs = scores.wrongs + 1
+                savedAnswers.push('wrongs')
+                wrongsAnswer.push(listAnswerSpan[i].innerText)
+            }
+            if(step == 1){
+            switch (listAnswerSpan[i].innerText) { //Выбор языка програмирования
+                case "Python":
+                    alert('hell1')
+                    
+                    break
+                case "Java":
+                    alert('hell2')
+                    step = step + 1
+                    break
+                case "Php":
+                    alert('hell3')
+                    break
+                case "C#":
+                    alert('hell4')
+                    break
+                case "Ruby":
+                    alert()
+                    break
+                default:
+                    break
+            }
+            }
+            console.log(wrongsAnswer);
+            console.log(scores);
+            if(step < 9){ //до это момента стоят чекбоксы
+                step = step + 1;
+            }
+            else{
+                showResult();
+            }
+            // console.log(step);
+            numberQuestion.innerText = step + 1
+            start();
+            // console.log(choice);
+        }
+    }
+    if(answerGiven == false){
+    alert('Вы не ответили')
+
+    }
+}
+
+
+const backQuestion = () =>{
+    for(let i = 0; i < listAnswer.length; i++){
+        if(listAnswer[i].checked == true){
+            answerGiven = true;
+            // choice = listAnswer[i].value
+            scores[savedAnswers[step - 1]] = scores[savedAnswers[step - 1]] - 1
+            savedAnswers.pop()
+            if(step != 0){ //здесь поставить ограничение на количество вопросов - 1
+                step = step - 1;
+            }
+            // console.log(step);
+            numberQuestion.innerText = step + 1
+            start();
+            // console.log(choice);
+        }
+    }
+}
+
+
+
+function showResult(){
+    alert('ТЫ прошёл тест!')
+    result.classList.remove('_active')
+    test.classList.add('_active')
+}
+further.addEventListener('click', furtherQuestion);
+back.addEventListener('click', backQuestion);
+
+
+
+
+// Добавить массив с темами и ответами
